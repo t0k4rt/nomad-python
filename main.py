8a8d92dff0b2a755d47b078fe0cdf6c97e039a5f
Alexandre Assouad <assouad@kairosfire.com>
2022-01-03 16:03:55 +0100

    new tests

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from icecream import ic
 import psycopg2
-import tabulate
+from tabulate import tabulate
 
 rng = np.random.default_rng()
 df = pd.DataFrame(rng.integers(0, 100, size=(100, 4)), columns=list('ABCD'))
