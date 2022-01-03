06c8d22f7f519baa2a157a9e38ada1e7addc431d
Alexandre Assouad <assouad@kairosfire.com>
2022-01-03 19:39:20 +0100

    new tests

--- nomad.sh
+++ nomad.sh
@@ -3,6 +3,7 @@
 set -e
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 export PATH="${PATH}:/nonexistent/.local/bin"
+pip3 list
 pip3 install -r "$SCRIPT_DIR/requirements.txt"
 
 echo $PATH
